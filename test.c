dd7ec084883b0921c6ae9f766bafb304255da08c
Quan Tran <tranminhquan97@gmail.com>
2013-09-03 22:28:04 +0700

    Stack overflow test

---
+++ test.c
@@ -0,0 +1,27 @@
+// buffer overflow test
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    char buffer1[8], buffer2[8];
+
+    strcpy(buffer1, "ab");
+    strcpy(buffer2, "456");
+
+    printf("BEFORE:\n");
+    printf("           Address            Value\n");
+    printf("buffer1    %p         %s\n", buffer1, buffer1);
+    printf("buffer2    %p         %s\n\n", buffer2, buffer2);
+
+    strcpy(buffer1, argv[1]);
+	
+    printf("AFTER COPY argv[1] INTO buffer1:\n");
+    printf("           Address            Value\n");
+    printf("buffer1    %p         %s\n", buffer1, buffer1);
+    printf("buffer2    %p         %s\n\n", buffer2, buffer2);
+
+    return 0;
+}

23f429a8e96cc84611d8ec12d2d1ffa33b104f30
Quan Tran <tranminhquan97@gmail.com>
2013-09-03 22:49:45 +0700

    Buffer overflow test

--- test.c
+++ test.c
@@ -6,27 +6,27 @@
 
 int main(int argc, char **argv)
 {
-    char *buffer1, *buffer2;
+	char *buffer1, *buffer2;
 
 	buffer1 = (char *) malloc(8);
 	buffer2 = (char *) malloc(8); 
 
-    strcpy(buffer1, "ab");
-    strcpy(buffer2, "456");
+	strcpy(buffer1, "ab");
+	strcpy(buffer2, "456");
 
-    printf("BEFORE:\n");
-    printf("           Address            Value\n");
-    printf("buffer1    %p         %s\n", buffer1, buffer1);
-    printf("buffer2    %p         %s\n\n", buffer2, buffer2);
+	printf("BEFORE:\n");
+	printf("           Address            Value\n");
+	printf("buffer1    %p         %s\n", buffer1, buffer1);
+	printf("buffer2    %p         %s\n\n", buffer2, buffer2);
 
-    strcpy(buffer1, argv[1]);
+	strcpy(buffer1, argv[1]);
 	
-    printf("AFTER COPY argv[1] INTO buffer1:\n");
-    printf("           Address            Value\n");
-    printf("buffer1    %p         %s\n", buffer1, buffer1);
-    printf("buffer2    %p         %s\n\n", buffer2, buffer2);
+	printf("AFTER COPY argv[1] INTO buffer1:\n");
+	printf("           Address            Value\n");
+	printf("buffer1    %p         %s\n", buffer1, buffer1);
+	printf("buffer2    %p         %s\n\n", buffer2, buffer2);
 
 	free(buffer1); free(buffer2);
 
-    return 0;
+	return 0;
 }
